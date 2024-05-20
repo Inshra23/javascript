9af4b43560a46ab2eece5b0721438a368e00e8dc
Inshra23 <164750803+Inshra23@users.noreply.github.com>
2024-05-20 16:07:41 +0500

    learningjavascript
    my practice

---
+++ script.js
@@ -0,0 +1,110 @@
+// let weight =  +prompt("Enter Weight")
+// let time = +prompt("Enter Time")
+// if(weight > 300 && time < 6){
+//     alert("Come to our tryout")
+// }
+// else{
+//     alert("come to our cookout")
+// }
+
+// let weight =  +prompt("Enter Weight")
+// let time = +prompt("Enter Time")
+// if(weight > 300 || time < 6){
+//     alert("Come to our tryout")
+// }
+// else{
+//     alert("come to our cookout")
+// }
+
+// let weight =  +prompt("Enter Weight")
+// let time = +prompt("Enter Time")
+// let age = +prompt("Enter Age")
+// let gender = +prompt("Male")
+// if(weight > 300 && time < 6 && age > 17 && gender === "male"){
+//     alert("Come to our tryout")
+// }
+// else{
+//     alert("come to our cookout")
+// }
+
+
+// let weight =  +prompt("Enter Weight")
+// let time = +prompt("Enter Time")
+// let age = +prompt("Enter Age")
+// let gender = +prompt("Male")
+// if(weight > 300 || time < 6 || age > 17 || gender === "male"){
+//     alert("Come to our tryout")
+// }
+// else{
+//     alert("come to our cookout")
+// }
+
+// let weight =  +prompt("Enter Weight")
+// let time = +prompt("Enter Time")
+// if(!(weight > 300 && time < 6)){
+
+//     //!(444 > 300 || 4 <6)
+//     alert("Come to our tryout")
+// }
+// else{
+//     alert("come to our cookout")
+// }
+
+// let x = 10;
+// let y = 5;
+// if (x > 5) {
+//     if (y > 2) {
+//         console.log("Both x and y are greater than their respective thresholds.");
+//     } else {
+//         console.log("x is greater than 5, but y is not greater than 2.");
+//     }
+// } else {
+//     console.log("x is not greater than 5, so the inner condition is not checked")
+    
+// }       
+
+// let temperature = 15;
+// let isSunny = true;
+// if(temperature > 25){
+//       console.log("Its hot outside");
+
+// if(isSunny){
+//     console.log("dont forget to wear sunscreen");
+// }
+// else{
+//     console.log("You might still want to wear sunscreen");
+// }
+// }
+// else if(temperature >= 15 && temperature <= 25){
+//     console.log("its a pleasant day");
+// if(isSunny){
+//     console.log("Enjoy the sunshine");
+// }
+// else{
+//     console.log("Even is its cloudy, its still nice outside");
+// }
+// }
+
+let age = 18
+let weight = 78
+if(age > 20){
+    if (weight > 45){
+        console.log("Your are in range");
+    }
+else{
+    console.log("You are out of range");
+}
+}
+else{
+   
+        console.log("Have a good weight");
+    }
+   
+
+
+
+
+
+
+    
+
